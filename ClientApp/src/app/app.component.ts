7f62347813a87117d9cab6d34acb7f52892d4f5b
St2r <St2r@github.com>
2020-06-07 12:44:51 +0800

    app-router改为模型驱动，forum组件完善路由刷新订阅功能

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -9,6 +9,22 @@ import {LoginService} from './services/login.service';
 export class AppComponent {
   public searchContent = '';
 
+  Subjects = [
+    ['离散数学', 'discrete-math'],
+    ['面向对象程序设计', 'oo-programming'],
+    ['系统编程', 'system-programming']
+  ];
+
+  SchoolLife = [
+    ['校卡招领', 'campus-card'],
+    ['课程推荐', 'course-recommend']
+  ];
+
+  AroundCampus = [
+    ['美食', 'food'],
+    ['游玩', 'amusement']
+  ];
+
   constructor() {
   }
 }
