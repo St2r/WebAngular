05064b133bb358a0332a76556928b183a3048e86
St2r <St2r@github.com>
2020-06-13 16:28:30 +0800

    评论展示和取评论数据的接口，我先去洗个澡，热死了

--- ClientApp/src/app/model/comment.ts
+++ ClientApp/src/app/model/comment.ts
@@ -1,13 +1,17 @@
 export interface Comment {
-  AuthorID: string;
-  AuthorName: string;
-  Content: string;
-  Likes: number;
-  Dislikes: number;
+  // 作者信息
+  username: string;
+  nickname: string;
+  avatarUrl: string;
+
+  content: string;
+  likes: number;
 
   // LikeStatus==0: 未被Like或者Dislike;
   // LikeStatus==1: 被该用户Like;
   // LikeStatus==-1: 被该用户Dislike;
-  LikeStatus: number;
-  CommentTime: Date;
+  likeStatus: number;
+
+  // yyyy-mm-dd-hh-mm
+  commentTime: string;
 }
