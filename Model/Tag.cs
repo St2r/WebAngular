bae3398aa14bc092037ea71d38eebb0e3fa127ce
维东 邱 <568385476@qq.com>
2020-06-14 01:46:08 +0800

    qiu

---
+++ Model/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAngular.Model
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+    }
+}
