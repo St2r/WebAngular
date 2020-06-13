fad6bdd9e9482493c21d575902e9fc41bce5efc8
Sky <Sky@buaa>
2020-06-13 23:01:34 +0800

    美化了一点点article view

--- ClientApp/src/app/pages/admin-manage/user-manage/user-manage.component.ts
+++ ClientApp/src/app/pages/admin-manage/user-manage/user-manage.component.ts
@@ -18,38 +18,7 @@ export class UserManageComponent implements OnInit {
 // TODO 获取数据
   loadUserInfo() {
     this.user_list = [
-      new class implements UserInfo {
-        // 用户名
-        username: 'test';
-        // 昵称
-        nickname: 'test';
-        // 头像地址
-        avatarUrl: 'fake';
-        // 个人简介
-        brief: 'test';
-  
-        // 我的关注数
-        follow: 2;
-        // 我的粉丝数
-        fans: 2;
-        // 用户登级
-        level: 2;
-        // 个人积分
-        point: 2;
-        // 用户发的帖子数量
-        articles: 2;
-        // 帖子被浏览数
-        browse: 2;
-        // 帖子被点赞数
-        like: 2;
-        // 帖子被收藏数
-        star: 2;
-  
-        // 该用户被登陆用户关注
-        isFollowed: true;
-        // 该用户关注了登陆用户
-        isFan: false;
-      }
+      
     ] 
     
   }
