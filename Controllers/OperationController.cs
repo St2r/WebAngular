eb125a8f44f0b7689594f2295505086f9c3ebf07
St2r <St2r@github.com>
2020-06-14 02:12:14 +0800

    .

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -23,38 +23,74 @@ namespace WebAngular.Controllers
         // 点赞-取消点赞-踩-取消踩
         // 绕口令八级
         
-        [HttpPost("/controller/operation/like")]
-        public bool Like([FromBody] OperationForm form)
+        [HttpPost("/controller/operation/like-article")]
+        public bool LikeArticle([FromBody] OperationForm form)
         {
             return false;
         }
         
-        [HttpPost("/controller/operation/undo-like")]
-        public bool UndoLike([FromBody] OperationForm form)
+        [HttpPost("/controller/operation/undo-like-article")]
+        public bool UndoLikeArticle([FromBody] OperationForm form)
         {
             return false;
         }
         
-        [HttpPost("/controller/operation/dislike")]
-        public bool Dislike([FromBody] OperationForm form)
+        [HttpPost("/controller/operation/dislike-article")]
+        public bool DislikeArticle([FromBody] OperationForm form)
         {
             return false;
         }
         
-        [HttpPost("/controller/operation/undo-dislike")]
-        public bool UndoDislike([FromBody] OperationForm form)
+        [HttpPost("/controller/operation/undo-dislike-article")]
+        public bool UndoDislikeArticle([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("/controller/operation/like-comment")]
+        public bool LikeComment([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("/controller/operation/undo-like-comment")]
+        public bool UndoLikeComment([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("/controller/operation/dislike-comment")]
+        public bool DislikeComment([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("/controller/operation/undo-dislike-comment")]
+        public bool UndoDislikeComment([FromBody] OperationForm form)
         {
             return false;
         }
 
-        [HttpPost("controller/operation/follow")]
-        public bool Follow([FromBody] OperationForm form)
+        [HttpPost("controller/operation/follow-person")]
+        public bool FollowPerson([FromBody] OperationForm form)
         {
             return false;
         }
         
-        [HttpPost("controller/operation/dis-follow")]
-        public bool DisFollow([FromBody] OperationForm form)
+        [HttpPost("controller/operation/dis-follow-person")]
+        public bool DisFollowPerson([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("controller/operation/follow-block")]
+        public bool FollowBlock([FromBody] OperationForm form)
+        {
+            return false;
+        }
+        
+        [HttpPost("controller/operation/dis-follow-block")]
+        public bool DisFollowBlock([FromBody] OperationForm form)
         {
             return false;
         }
